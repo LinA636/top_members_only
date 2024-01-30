b30ba61fd64c12882406ee907be92758924ff675
Lina <117147821+LinA636@users.noreply.github.com>
2024-01-30 13:44:45 +1300

    link a welcome page to being shown after initial sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  private
+  
+  def after_sign_in_path_for(resource)
+    welcome_path
+  end
+
 end
