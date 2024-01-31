7e2cada919e4105a89b56fa154409e952cb26a9b
Lina <117147821+LinA636@users.noreply.github.com>
2024-01-31 13:23:45 +1300

    add name field for sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_232628) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_31_001114) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -20,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_232628) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "confrimable"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
