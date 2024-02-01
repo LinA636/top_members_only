b0f3731f359de67303ebd065e8b7848f109f8e8d
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-01 13:11:27 +1300

    solve post database problems

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_31_192315) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_31_224755) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "content"
   end
 
   create_table "users", force: :cascade do |t|
