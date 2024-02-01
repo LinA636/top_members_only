ae800730a819e95df32e02e15013f923cc879a51
Lina <117147821+LinA636@users.noreply.github.com>
2024-02-01 13:22:52 +1300

    let signed in user create a new post and show it

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+    before_action :require_login, only: [:new, :create]
+
+    def index 
+        @posts = Post.all
+    end
+
+    def new 
+        @post = Post.new
+    end
+
+    def create 
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post, notice: 'Post was successfully created.'
+        else
+            render :new,  status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    private
+
+    def require_login
+        if user_signed_in?
+            # allow the user to create new posts
+        else
+            redirect_to user_session_path
+        end
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+
+    # Add any associations if your Post model is related to other models
+    # belongs_to :user
 end
